157b0e746a92e49fa1b7cff71d5cd4b908a05dc3
wingsico <wingsico@gmail.com>
2018-12-22 09:53:01 +0800

    [update]: 新增信息展示区域单条信息组件

---
+++ src/components/common/InfoWrapper.vue
@@ -0,0 +1,50 @@
+<template>
+  <mu-ripple class="info-wrapper" @click="routerGo()">
+    <p class="content">{{ content }}</p>
+    <p class="desc">{{ desc }}</p>
+  </mu-ripple>
+</template>
+
+<script>
+import { isUndefined } from 'lodash';
+export default {
+  name: "info-wrapper",
+  props: {
+    content: String,
+    desc: String,
+    to: String,
+  },
+  methods: {
+    routerGo() {
+      if (isUndefined(this.to)) {
+        return;
+      }
+
+      this.$router.push(this.to);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.info-wrapper {
+  padding: 30px 0;
+  padding-left: 40px;
+  letter-spacing: 0.5px;
+  position: relative;
+  .content {
+    color: #000;
+    font-size: 14px;
+    font-weight: 300;
+  }
+  .desc {
+    color: #888;
+    font-weight: 300;
+    font-size: 12px;
+    margin-top: -3px;
+  }
+  p {
+    margin: 0;
+  }
+}
+</style>
