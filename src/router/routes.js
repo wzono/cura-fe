81903c9c6704fb32b173ce91924d91e66b8e9e12
wingsico <wingsico@gmail.com>
2018-12-22 09:46:07 +0800

    [update]: 新增路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,18 @@
 
-const Login = () => import('@/views/login/index.vue');
-const Welcome = () => import('@/views/welcome/index.vue');
-const Register = () => import('@/views/register/index.vue');
-const RegisterSuccess = () => import('@/views/register/success.vue');
+const Login = () => import('@/views/login/Index.vue');
+const Welcome = () => import('@/views/welcome/Index.vue');
+const Register = () => import('@/views/register/Index.vue');
+const RegisterSuccess = () => import('@/views/register/Success.vue');
 
-const UserLayout = () => import('@/views/user/layout.vue');
-const UserMessageBox = () => import('@/views/user/components/msg_box.vue');
-const UserContacts = () => import('@/views/user/components/contacts');
-const UserChat = () => import('@/views/user/components/chat');
-const UserAddContact = () => import('@/views/user/components/add_contact');
+const UserLayout = () => import('@/views/user/Layout.vue');
+const UserMessageBox = () => import('@/views/user/components/MessageBox.vue');
+const UserContacts = () => import('@/views/user/components/Contacts');
+const UserChat = () => import('@/views/user/components/Chat');
+const UserSearchContact = () => import('@/views/user/components/SearchContact');
+const UserDetail = () => import('@/views/user/components/UserDetail');
+const UserAddContact = () => import('@/views/user/components/AddContact');
+
+const UserSettings = () => import('@/views/user/components/Settings');
 
 
 const routes = [
@@ -56,9 +60,24 @@ const routes = [
         component: UserChat,
       },
       {
+        path: 'search_contact',
+        name: 'search-contact',
+        component: UserSearchContact,
+      },
+      {
+        path: 'detail',
+        name: 'user-detial',
+        component: UserDetail,
+      },
+      {
         path: 'add_contact',
         name: 'add-contact',
         component: UserAddContact,
+      },
+      {
+        path: 'settings',
+        name: 'settings',
+        component: UserSettings,
       }
     ]
   }
