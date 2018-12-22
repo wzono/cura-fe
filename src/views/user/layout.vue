8425c18e87e7595c59bcff900acdfb96b18bc9ad
wingsico <wingsico@gmail.com>
2018-12-22 09:57:11 +0800

    [update]: 初始获取分组

--- src/views/user/layout.vue
+++ src/views/user/layout.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="user-page">
     <CommonFadeTransition>
-      <router-view />
+      <router-view/>
     </CommonFadeTransition>
   </div>
 </template>
 
 <script>
+import {mapActions} from "vuex";
+
 export default {
   name: "user-page",
-
+  methods: {
+    ...mapActions(['getGroups'])
+  },
+  created() {
+    this.getGroups();
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-
 </style>
