72b98f765d4e388ce7265fcb362aeba5c3a241ab
wingsico <wingsico@gmail.com>
2018-12-22 09:43:31 +0800

    [update]: 修改&更新mock数据

--- mock/index.js
+++ mock/index.js
@@ -16,7 +16,7 @@ module.exports = app => {
   })
 
   app.get('/api/user/me', (req, res) => {
-    const json = getJSONData('./templates/login.json');
+    const json = getJSONData('./templates/me.json');
     res.json(Mock.mock(json));
   })
 
@@ -25,9 +25,72 @@ module.exports = app => {
     res.json(Mock.mock(json));
   })
 
-  app.get('/api/user/search_contacts', (req, res) => {
+  app.get('/api/user/get_users_by_imformation', (req, res) => {
     console.log(req.query);
     const json = getJSONData('./templates/search_contacts.json');
     res.json(Mock.mock(json));
+  })
+
+  app.get('/api/user/get_user', (req, res) => {
+    console.log(req.query);
+    const json = getJSONData('./templates/user.json');
+    res.json(Mock.mock(json));
+  })
+
+  app.get('/api/user/groups', (req, res) => {
+    const json = getJSONData('./templates/groups.json');
+    res.json(Mock.mock(json));
+  })
+
+  app.post('/api/friend/add_friend', (req, res) => {
+    res.json({
+      status: 200,
+      message: 'ok'
+    })
+  })
+
+  app.delete('/api/friend/delete_friend', (req, res) => {
+    res.json({
+      status: 200,
+      message: 'ok'
+    })
+  })
+
+  app.post('/api/user/add_commend', (req, res) => {
+    res.json({
+      status: 200,
+      message: 'ok'
+    })
+  })
+
+  app.delete('/api/user/delete_commend', (req, res) => {
+    res.json({
+      status: 200,
+      message: 'ok'
+    })
+  })
+
+  app.put('/api/user/update_basic_info', (req, res) => {
+    res.json({
+      status: 200,
+      message: 'ok',
+    })
+  })
+
+  app.put('/api/user/update_head', (req, res) => {
+    res.json({
+      status: 200,
+      message: 'ok',
+      data: {
+        headUrl: "http://cdn.wingsico.org/image/avatar/181020-0.jpeg"
+      }
+    })
+  })
+
+  app.put('/api/user/update_phone', (req, res) => {
+    res.json({
+      status: 200,
+      message: 'ok',
+    })
   })
 };
